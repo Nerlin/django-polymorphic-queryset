c8567d79d0fd3abfa204cf90a8276b6868cd96ad
nerlin <nerlin57@gmail.com>
2018-07-29 04:46:09 +0300

    Removed redundant queryset methods.

--- polymorphic_queryset/__init__.py
+++ polymorphic_queryset/__init__.py
@@ -38,93 +38,69 @@ class Queryable(object, metaclass=QueryableMeta):
     inheritors = []
     model_name = None
 
-    @classmethod
-    def add_inheritor(cls, inheritor):
-        if issubclass(inheritor, cls):
-            cls.inheritors.append(cls)
-        else:
-            raise TypeError("Inheritor of this class must be the same class or subclass.")
 
-    @classmethod
-    def remove_inheritor(cls, inheritor):
-        cls.inheritors.remove(inheritor)
+def query(distinct=False):
+    def query_wrapper(queryset_method):
+        def query_decorator(queryset, *args, **kwargs):
+            query_name = queryset_method.__name__
 
-    @classmethod
-    def conditions(cls, query, *args, **kwargs):
-        conditions = query(cls, *args, **kwargs)
-        distinct = query.distinct
+            def query_traverse(queryset_cls):
+                conditions_method = getattr(queryset_cls, query_name)
+                conditions = conditions_method.conditions(queryset_cls, *args, **kwargs)
+                distinct = conditions_method.distinct
 
-        for inheritor_cls in cls.inheritors:
-            queryset_conditions = query.default_conditions
-            query_name = query.__name__
-            query_method = getattr(inheritor_cls, query_name, None)
-
-            if query_method is not query:
-                queryset_conditions, distinct = inheritor_cls.conditions(
-                    query_method.conditions, *args, **kwargs
-                )
-                if not queryset_conditions:
+                for inheritor_cls in queryset_cls.inheritors:
                     queryset_conditions = query.default_conditions
+                    query_method = getattr(inheritor_cls, query_name, None)
+                    if query_method is not conditions_method:
+                        queryset_conditions, child_distinct = query_traverse(inheritor_cls)
+                        if not queryset_conditions:
+                            queryset_conditions = query.default_conditions
+                        if child_distinct:
+                            distinct = True
 
-                if not isinstance(queryset_conditions, Q):
-                    raise ValueError(
-                        "Filter '{query_name}' for '{inheritor_class_name}' returned not a Q object.".format(
-                            query_name=query_name,
-                            inheritor_class_name=inheritor_cls.__name__
-                        )
-                    )
+                        if not isinstance(queryset_conditions, Q):
+                            raise ValueError(
+                                "Filter '{query_name}' for '{inheritor_class_name}' returned not a Q object.".format(
+                                    query_name=query_name,
+                                    inheritor_class_name=inheritor_cls.__name__
+                                )
+                            )
 
-            query_model_name = inheritor_cls.model_name.lower()
-            cls.rename_conditions_to_inherited_model(queryset_conditions, query_model_name)
+                    query_model_name = inheritor_cls.model_name.lower()
+                    rename_conditions(queryset_conditions, query_model_name)
 
-            if conditions is None:
-                conditions = query.default_conditions
-            if queryset_conditions != query.default_conditions:
-                inherited_model_cls_not_included = query_model_name + "__isnull"
-                conditions &= Q(**{inherited_model_cls_not_included: True})
-                conditions |= (
-                    Q(**{inherited_model_cls_not_included: False}) & queryset_conditions
-                )
-        return conditions, distinct
+                    if conditions is None:
+                        conditions = query.default_conditions
+                    if queryset_conditions != query.default_conditions:
+                        inherited_model_cls_not_included = query_model_name + "__isnull"
+                        conditions &= Q(**{inherited_model_cls_not_included: True})
+                        conditions |= Q(**{inherited_model_cls_not_included: False}) & queryset_conditions
+                return conditions, distinct
 
-    @classmethod
-    def rename_conditions_to_inherited_model(cls, queryset_conditions, query_model_name):
-        for index, child in enumerate(queryset_conditions.children):
-            if isinstance(child, Node):
-                cls.rename_conditions_to_inherited_model(child, query_model_name)
-            else:
-                param_name, param_value = child
-                param_key = query_model_name + "__" + param_name
-                queryset_conditions.children[index] = param_key, param_value
-        return queryset_conditions
-
-
-class query(object):
-    @staticmethod
-    def none():
-        return Q(pk=-1)
-
-    @staticmethod
-    def default_conditions():
-        return Q()
-
-    @staticmethod
-    def all():
-        return query.default_conditions()
-
-    def __init__(self, distinct=False):
-        self.distinct = distinct
-
-    def __call__(self, base_method):
-        def query_wrapper(queryset, *args, **kwargs):
-            conditions, distinct = queryset.conditions(base_method, *args, **kwargs)
-
-            results = queryset.filter(conditions)
+            query_conditions, distinct = query_traverse(queryset)
+            queryset = queryset.filter(query_conditions)
             if distinct:
-                results = results.distinct()
-            return results
+                queryset = queryset.distinct()
+            return queryset
 
-        base_method.distinct = self.distinct
-        base_method.default_conditions = query.default_conditions
-        query_wrapper.conditions = base_method
-        return query_wrapper
+        query_decorator.conditions = queryset_method
+        query_decorator.distinct = distinct
+        return query_decorator
+    return query_wrapper
+
+
+query.default_conditions = lambda: Q()
+query.all = lambda: Q()
+query.none = lambda: Q(pk=-1)
+
+
+def rename_conditions(conditions, model_name):
+    for index, child in enumerate(conditions.children):
+        if isinstance(child, Node):
+            rename_conditions(child, model_name)
+        else:
+            param_name, param_value = child
+            param_key = model_name + "__" + param_name
+            conditions.children[index] = param_key, param_value
+    return conditions
